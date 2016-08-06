ace9ac4e2bde0b7aa1ca85f41064599d3324101c
Nosthertus <nosthertus@gmail.com>
2016-08-06 04:01:41 -0400

    ADDED: Clear, Flush history functions
    *Added Clear function to empty the whole history object
    *Added Flush function to remove any additional entry if the object length is bigger than max entries property

--- socket/events.js
+++ socket/events.js
@@ -3,28 +3,42 @@ var moment  = require("moment");
 
 module.exports = function(io){
 	var history = new History({
-		timeFormat: "YYYY-MM-DD HH:mm:ss"
+		time_format: "YYYY-MM-DD HH:mm:ss"
 	});
 
 	io.on('connection', function(socket){
 		console.log(moment().format("HH:mm:ss") + ' new socket connected');
 
 		var user = {
-			name: socket.handshake.query.username
+			name: socket.handshake.query.username,
+			id: socket.id
 		};
 
-		io.emit("connection.start", {
+		var session = {
 			username: user.name,
-			time: moment().format("HH:mm:ss")
+			message: "user logged in",
+			type: "session.start"
+		};
+
+		if(history.add_entry(session)){
+			io.emit("connection.start", {
+				username: user.name,
+				time: moment().format("HH:mm:ss")
+			});
+		}
+
+		socket.on("debug.history", function(data){
+			socket.emit("debug.history", history.get_all());
 		});
 
 		socket.on('message:new', function(data){
 			var msg = {
 				username: user.name,
 				message: data,
+				type: "message.normal"
 			};
 
-			if(obj = history.addItem(msg)){
+			if(obj = history.add_entry(msg)){
 				Object.assign(msg, obj);
 			}
 
